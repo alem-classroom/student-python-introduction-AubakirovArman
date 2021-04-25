8ed0f4cbdcf35dd1e124405cc51d1e5e5734e6d1
AubakirovArman <armanibadboy@mail.ru>
2021-04-25 18:15:22 +0600

    done

--- lists/lists.py
+++ lists/lists.py
@@ -6,12 +6,11 @@ def add_elem_to_list(list, elem):
     return list
 
 def delete_elem_from_list(list, index = -1):
-    if index<len(list) and (len(list)*-1)<index:
+    try:
         list.pop(index)
-        return list
-    else:
-        li=[]
-        return li
+        return list 
+    except:
+        return []
 
 def count_elements_in_list(list, x):
     return list.count(x)
