d606b7d574b288e7ee79b5faef240fb7182be599
AubakirovArman <armanibadboy@mail.ru>
2021-04-25 18:50:14 +0600

    d

--- lists/lists.py
+++ lists/lists.py
@@ -1,28 +1,31 @@
 def size_of_list(list):
     return len(list)
+    # return sizt
 
 def add_elem_to_list(list, elem):
     list.append(elem)
     return list
+    # add elem to list and ret
 
-def delete_elem_from_list(list, index = -1):
+def delete_elem_from_list(list, index = -1): 
     if index != -1 and len(list) > index:
         return list.pop(index)
 
     else:
         return list.pop()
+  
+    # delete element from list, such that its index is index
+
 
 def count_elements_in_list(list, x):
     return list.count(x)
-
+    # count elements in the list that are equal to x 
 
 def sort_list(list):
     list = list.sort()
     return list
+    # return sor
 
 def reverse(list):
     list = list.reverse()
-    return list
-
-
-print(reverse([1,12,3]))
+    # return reversed
