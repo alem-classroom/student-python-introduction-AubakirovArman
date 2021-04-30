0f7506d40e054f3da207abe139d04ddd443606c6
Arman <32128949+AubakirovArman@users.noreply.github.com>
2021-04-30 22:24:58 +0600

    Update lists.py

--- lists/lists.py
+++ lists/lists.py
@@ -9,14 +9,10 @@ def add_elem_to_list(list, elem):
 
 def delete_elem_from_list(list, index = -1):
 
-    if index < 0:
-        index = len(list) + index
-
-    if (index >= 0) and (index >= len(list)):
+    if(-len(list)<=index<len(list)):
+        return list.pop(index)
+    else:
         return []
-
-    x = list.pop(index)
-    return list
 
     # delete element from list, such that its index is index
     # if index is invalid, return empty list
